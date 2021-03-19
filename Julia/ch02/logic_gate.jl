730030c60b582091cc9d623c7a9cba46e81bf697
Loliver <swim.01060808@gmail.com>
2021-03-20 01:35:49 +0900

    :recycle: ユニットテストを標準ので書き直し

--- Julia/ch02/logic_gate.jl
+++ Julia/ch02/logic_gate.jl
@@ -1,3 +1,5 @@
+using Test
+
 function AND(x1, x2)
     x = [x1, x2]
     w = [0.5, 0.5]
@@ -30,38 +32,31 @@ function XOR(x1, x2)
 end
 
 function main()
-    println("AND test")
-    testAND = [(0, 0, 0), (1, 0, 0), (0, 1, 0), (1, 1, 1)]
-    test(AND, testAND)
-
-    println("NAND test")
-    testNAND = [(0, 0, 1), (1, 0, 1), (0, 1, 1), (1, 1, 0)]
-    test(NAND, testNAND)
-
-    println("OR test")
-    testOR = [(0, 0, 0), (1, 0, 1), (0, 1, 1), (1, 1, 1)]
-    test(OR, testOR)
-
-    println("XOR test")
-    testXOR = [(0, 0, 0), (1, 0, 1), (0, 1, 1), (1, 1, 0)]
-    test(XOR, testXOR)
-end
-
-function test(f, cases)
-    cnt_error = 0
-    for (x1, x2, want) in cases
-        res = f(x1, x2)
-        if res == want
-            println("-- PASS")
-        else
-            print("-- FAIL -- return $res but want = $want")
-            cnt_error += 1
+    @testset "Test" begin
+        @testset "AND" begin
+            @test AND(0, 0) == 0
+            @test AND(0, 1) == 0
+            @test AND(1, 0) == 0
+            @test AND(1, 1) == 1
         end
-    end
-    if cnt_error == 0
-        println("ok")
-    else
-        println("failed in $cnt_error cases")
+        @testset "NAND" begin
+            @test NAND(0, 0) == 1
+            @test NAND(0, 1) == 1
+            @test NAND(1, 0) == 1
+            @test NAND(1, 1) == 0
+        end
+        @testset "OR" begin
+            @test OR(0, 0) == 0
+            @test OR(0, 1) == 1
+            @test OR(1, 0) == 1
+            @test OR(1, 1) == 1
+        end
+        @testset "XOR" begin
+            @test XOR(0, 0) == 0
+            @test XOR(0, 1) == 1
+            @test XOR(1, 0) == 1
+            @test XOR(1, 1) == 0
+        end
     end
 end
 
